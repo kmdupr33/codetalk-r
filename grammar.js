ee3857a21882517c5fcb94f1f693f3cda82bb415
Matt Dupree <kmdupr33@gmail.com>
2019-06-07 20:32:47 -0400

    trim parse results to something useful

--- grammar.js
+++ grammar.js
@@ -3,44 +3,71 @@
 (function () {
 function id(x) { return x[0]; }
 
-  function nuller() {
-    return null;
-  }
+// Bypasses TS6133. Allow declared but unused functions.
+// @ts-ignore
+function nth(n) {
+    return function(d) {
+        return d[n];
+    };
+}
+
+
+// Bypasses TS6133. Allow declared but unused functions.
+// @ts-ignore
+function $(o) {
+    return function(d) {
+        var ret = {};
+        Object.keys(o).forEach(function(k) {
+            ret[k] = d[o[k]];
+        });
+        return ret;
+    };
+}
 var grammar = {
     Lexer: undefined,
     ParserRules: [
-    {"name": "CodeTalk", "symbols": ["CodeTalkKeyWord", "Newline", "Slides", "Done"]},
-    {"name": "CodeTalkKeyWord$string$1", "symbols": [{"literal":"c"}, {"literal":"o"}, {"literal":"d"}, {"literal":"e"}, {"literal":"t"}, {"literal":"a"}, {"literal":"l"}, {"literal":"k"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "CodeTalkKeyWord", "symbols": ["CodeTalkKeyWord$string$1"]},
-    {"name": "Slides$ebnf$1", "symbols": ["Slide"]},
-    {"name": "Slides$ebnf$1", "symbols": ["Slides$ebnf$1", "Slide"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "Slides", "symbols": ["Slides$ebnf$1"]},
+    {"name": "CodeTalk$string$1", "symbols": [{"literal":"c"}, {"literal":"o"}, {"literal":"d"}, {"literal":"e"}, {"literal":"t"}, {"literal":"a"}, {"literal":"l"}, {"literal":"k"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "CodeTalk$ebnf$1", "symbols": ["Slide"]},
+    {"name": "CodeTalk$ebnf$1", "symbols": ["CodeTalk$ebnf$1", "Slide"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "CodeTalk", "symbols": ["CodeTalk$string$1", "Newline", "CodeTalk$ebnf$1", "Done"], "postprocess":  
+        data => [data[0]].concat(data[2]).concat(data[3][0]) 
+        },
     {"name": "Slide$string$1", "symbols": [{"literal":"s"}, {"literal":"l"}, {"literal":"i"}, {"literal":"d"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Slide$ebnf$1", "symbols": ["Word"]},
-    {"name": "Slide$ebnf$1", "symbols": ["Slide$ebnf$1", "Word"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "Slide", "symbols": ["Indent", "Slide$string$1", "Newline", "Indent", "Indent", "Slide$ebnf$1", "Indent", "EndSlide"]},
+    {"name": "Slide", "symbols": ["Indent", "Slide$string$1", "Newline", "Markdown", "Indent", "EndSlide"], "postprocess":  
+        data => [data[1]].concat(data[3]).concat(data[5])
+        },
+    {"name": "Markdown$ebnf$1", "symbols": ["Block"]},
+    {"name": "Markdown$ebnf$1", "symbols": ["Markdown$ebnf$1", "Block"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Markdown", "symbols": ["Markdown$ebnf$1"]},
     {"name": "EndSlide$string$1", "symbols": [{"literal":"e"}, {"literal":"n"}, {"literal":"d"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "EndSlide$ebnf$1", "symbols": ["Newline"]},
     {"name": "EndSlide$ebnf$1", "symbols": ["EndSlide$ebnf$1", "Newline"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "EndSlide", "symbols": ["EndSlide$string$1", "EndSlide$ebnf$1"]},
-    {"name": "Newline", "symbols": [{"literal":"\n"}], "postprocess": nuller},
-    {"name": "Indent$string$1", "symbols": [{"literal":" "}, {"literal":" "}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Indent", "symbols": ["Indent$string$1"], "postprocess": nuller},
-    {"name": "Space", "symbols": [{"literal":" "}]},
+    {"name": "EndSlide", "symbols": ["EndSlide$string$1", "EndSlide$ebnf$1"], "postprocess": id},
+    {"name": "Block$ebnf$1", "symbols": ["Word"]},
+    {"name": "Block$ebnf$1", "symbols": ["Block$ebnf$1", "Word"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Block$ebnf$2", "symbols": []},
+    {"name": "Block$ebnf$2", "symbols": ["Block$ebnf$2", "Newline"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Block", "symbols": ["Indent", "Indent", "Block$ebnf$1", "Block$ebnf$2"], "postprocess":  
+        data => data[2].join(' ') 
+        },
     {"name": "Word$ebnf$1", "symbols": ["Letter"]},
     {"name": "Word$ebnf$1", "symbols": ["Word$ebnf$1", "Letter"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "Word$subexpression$1", "symbols": ["Space"]},
     {"name": "Word$subexpression$1", "symbols": ["Newline"]},
-    {"name": "Word", "symbols": ["Word$ebnf$1", "Word$subexpression$1"], "postprocess":  function(data) {
-          return data[0].join("")
-        } },
+    {"name": "Word", "symbols": ["Word$ebnf$1", "Word$subexpression$1"], "postprocess":  
+        data => data[0].join("")
+        },
     {"name": "Letter", "symbols": [/[a-z]/], "postprocess": id},
     {"name": "Done$string$1", "symbols": [{"literal":"e"}, {"literal":"n"}, {"literal":"d"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "Done$ebnf$1", "symbols": []},
     {"name": "Done$ebnf$1$subexpression$1", "symbols": ["Space"]},
     {"name": "Done$ebnf$1$subexpression$1", "symbols": ["Newline"]},
     {"name": "Done$ebnf$1", "symbols": ["Done$ebnf$1", "Done$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "Done", "symbols": ["Done$string$1", "Done$ebnf$1"]}
+    {"name": "Done", "symbols": ["Done$string$1", "Done$ebnf$1"]},
+    {"name": "Newline", "symbols": [{"literal":"\n"}]},
+    {"name": "Indent$string$1", "symbols": [{"literal":" "}, {"literal":" "}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "Indent", "symbols": ["Indent$string$1"]},
+    {"name": "Space", "symbols": [{"literal":" "}]}
 ]
   , ParserStart: "CodeTalk"
 }
--- grammar.ne
+++ grammar.ne
@@ -1,20 +1,31 @@
-CodeTalk -> CodeTalkKeyWord Newline Slides Done
-CodeTalkKeyWord -> "codetalk"
-Slides -> Slide:+
-Slide -> Indent "slide" Newline Indent Indent Word:+ Indent EndSlide
-EndSlide -> "end" Newline:+
-Newline -> "\n" {% nuller %}
-Indent -> "  " {% nuller %}
-Space -> " "
-Word -> Letter:+ (Space | Newline) {% function(data) {
-  return data[0].join("")
-} %}
+@builtin "postprocessors.ne"
+
+CodeTalk -> "codetalk" Newline Slide:+ Done {% 
+  data => [data[0]].concat(data[2]).concat(data[3][0]) 
+%}
+
+Slide -> Indent "slide" Newline Markdown Indent EndSlide {% 
+  data => [data[1]].concat(data[3]).concat(data[5])
+%}
+
+Markdown -> Block:+
+EndSlide -> "end" Newline:+ {% id %}
+# a new line ends a word AND a block
+# if it ends a word, then you can have multiple letters at different indents
+# if it only ends a block, then you have to leave a trailing space, which sucks
+# if Letter+ (Space | Newline), then either end or Block
+# TODO return and fix this, probably going to need a lexer and/or to leverage the reject feature (abandone the hope of this being a CFG)
+Block -> Indent Indent Word:+ Newline:* {% 
+  data => data[2].join(' ') 
+%}
+Word -> Letter:+ (Space | Newline) {% 
+  data => data[0].join("")
+%}
 Letter -> [a-z] {% id %}
 Done -> "end" (Space | Newline):*
 
 
-@{%
-  function nuller() {
-    return null;
-  }
-%}
+# Whitespace
+Newline -> "\n"
+Indent -> "  "
+Space -> " "
