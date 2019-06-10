aacadd03134f4bda9d110f6bfb65a6b78fa61502
Matt Dupree <kmdupr33@gmail.com>
2019-06-10 18:51:33 -0400

    change codetalkc so that it accepts a path to a source file

--- packages/compiler/codetalkc.js
+++ packages/compiler/codetalkc.js
@@ -1,23 +1,14 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
 const parser = require("./index");
 
-let codetalkSource = "";
-process.stdin.setEncoding('utf8');
-process.stdin.on("readable", () => {
-  let chunk;
-  // Use a loop to make sure we read all available data.
-  while ((chunk = process.stdin.read()) !== null) {
-    codetalkSource += chunk;
-  }
-});
+const path = process.argv[2]
+const source = fs.readFileSync(path, 'utf8');
+parser.feed(source);
+if (parser.results.length === 0) {
+  console.error("unexpected end of input");
+  return -1;
+}
 
-process.stdin.on("end", () => {
-  parser.feed(codetalkSource);
-  if (parser.results.length === 0) {
-    console.error("unexpected end of input");
-    return -1;
-  }
-
-  console.log(JSON.stringify(parser.results));
-});
+console.log(JSON.stringify(parser.results));
