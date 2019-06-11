a04fefac5a3f523bfdd2fe0b1fdc1713c20ac136
Matt Dupree <kmdupr33@gmail.com>
2019-06-10 20:35:34 -0400

    remove runtime

--- packages/codetalk-runtime/example/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ExampleComponent from 'codetalk-runtime';
-
-ReactDOM.render(<ExampleComponent text="Hello"/>, document.getElementById('root'))
